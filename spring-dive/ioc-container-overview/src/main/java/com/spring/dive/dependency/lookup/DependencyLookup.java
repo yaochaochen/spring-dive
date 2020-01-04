260310da8c8cf5fded5f3e0d47c39aeb8df34cf1
姚超臣 <yaochaochen@ctv.org.cn>
2020-01-04 14:50:16 +0800

    spring container lazy

--- spring-dive/ioc-container-overview/src/main/java/com/spring/dive/dependency/lookup/DependencyLookup.java
+++ spring-dive/ioc-container-overview/src/main/java/com/spring/dive/dependency/lookup/DependencyLookup.java
@@ -2,6 +2,7 @@ package com.spring.dive.dependency.lookup;
 
 import com.spring.dive.dependency.domain.User;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -13,6 +14,13 @@ public class DependencyLookup {
         BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-lookup-context.xml");
 
         lookupRealTime(beanFactory);
+        lookupLazy(beanFactory);
+    }
+
+    private static void lookupLazy(BeanFactory beanFactory) {
+      ObjectFactory<User> userObjectFactory = (ObjectFactory<User>) beanFactory.getBean("objectFactory");
+      User user = userObjectFactory.getObject();
+        System.out.println("延时查找" + user);
     }
 
     private static void lookupRealTime(BeanFactory beanFactory) {
