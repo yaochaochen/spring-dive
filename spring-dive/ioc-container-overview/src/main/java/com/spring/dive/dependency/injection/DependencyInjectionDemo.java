90c776c5d88a8e0efa2cacb5c1fe4b5719e49877
姚超臣 <yaochaochen@ctv.org.cn>
2020-01-08 14:49:24 +0800

    spring container beans 依赖注入来源

--- spring-dive/ioc-container-overview/src/main/java/com/spring/dive/dependency/injection/DependencyInjectionDemo.java
+++ spring-dive/ioc-container-overview/src/main/java/com/spring/dive/dependency/injection/DependencyInjectionDemo.java
@@ -1,8 +1,10 @@
 package com.spring.dive.dependency.injection;
 
 import com.spring.dive.dependency.repository.UserRepository;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.env.Environment;
 
 /**
  * 依赖注入
@@ -13,8 +15,18 @@ public class DependencyInjectionDemo {
     public static void main(String[] args) {
 
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-injection-context.xml");
-        //依赖来源-:
+        BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-injection-context.xml");
+        //依赖来源-:自定义 Bean
         UserRepository userRepository = applicationContext.getBean("userRepository", UserRepository.class);
         System.out.println(userRepository.getUsers());
+
+        //依赖来源二 依赖注入 内建依赖
+        System.out.println(userRepository.getObjectFactory());
+
+        //容器内建  Bean
+        Environment environment =  beanFactory.getBean(Environment.class);
+        System.out.println(environment.toString());
+
+
     }
 }
