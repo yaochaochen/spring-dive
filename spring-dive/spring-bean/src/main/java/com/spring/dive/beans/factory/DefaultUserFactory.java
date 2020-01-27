9e172c4827496895f45edf1f6e8b38fb56328717
姚超臣 <yaochaochen@ctv.org.cn>
2020-01-27 14:45:39 +0800

    BeanDefinition Bean 销毁

--- spring-dive/spring-bean/src/main/java/com/spring/dive/beans/factory/DefaultUserFactory.java
+++ spring-dive/spring-bean/src/main/java/com/spring/dive/beans/factory/DefaultUserFactory.java
@@ -9,4 +9,8 @@ public class DefaultUserFactory implements UserFactory {
     public void  init(){
         System.out.println("PostConstruct : 初始化中..." );
     }
+
+    public  void  UserFactoryInit() {
+        System.out.println("PostConstruct : 初始化中..." );
+    }
 }
