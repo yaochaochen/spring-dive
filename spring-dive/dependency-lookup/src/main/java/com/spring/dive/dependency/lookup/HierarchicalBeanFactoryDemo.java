80824d4f76049769be5c2fc57876263caafa21f3
姚超臣 <yaochaochen@ctv.org.cn>
2020-01-29 15:53:31 +0800

    Object 层次性 lookup

--- spring-dive/dependency-lookup/src/main/java/com/spring/dive/dependency/lookup/HierarchicalBeanFactoryDemo.java
+++ spring-dive/dependency-lookup/src/main/java/com/spring/dive/dependency/lookup/HierarchicalBeanFactoryDemo.java
@@ -1,17 +1,31 @@
 package com.spring.dive.dependency.lookup;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class HierarchicalBeanFactoryDemo {
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
-        applicationContext.register(HierarchicalBeanFactoryDemo.class);
+        applicationContext.register(ObjectProviderDemo.class);
+
+        ConfigurableListableBeanFactory beanFactory = applicationContext.getBeanFactory();
+        System.out.println("当前 Bean 的 parent ： " + beanFactory.getParentBeanFactory());
+
+        beanFactory.setParentBeanFactory(createBeanFactory());
+        System.out.println("当前 Bean 的 BeanFactory ： " + beanFactory.getParentBeanFactory());
         applicationContext.refresh();
-        lookupObjectProvider(applicationContext);
         applicationContext.close();
     }
 
-    private static void lookupObjectProvider(AnnotationConfigApplicationContext applicationContext) {
+    private static BeanFactory createBeanFactory() {
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
+        String location = "classpath:/META-INF/dependency-lookup-context.xml";
+        reader.loadBeanDefinitions(location);
+        return beanFactory;
     }
 }
