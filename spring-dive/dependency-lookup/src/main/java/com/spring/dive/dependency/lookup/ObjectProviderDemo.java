f05752c210cfebe74d37fa2b1d3bc1da6fe86f4d
姚超臣 <yaochaochen@ctv.org.cn>
2020-02-03 20:42:35 +0800

    lookupAvailable

--- spring-dive/dependency-lookup/src/main/java/com/spring/dive/dependency/lookup/ObjectProviderDemo.java
+++ spring-dive/dependency-lookup/src/main/java/com/spring/dive/dependency/lookup/ObjectProviderDemo.java
@@ -1,5 +1,6 @@
 package com.spring.dive.dependency.lookup;
 
+import com.spring.dive.dependency.domain.User;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +12,17 @@ public class ObjectProviderDemo {
         applicationContext.register(ObjectProviderDemo.class);
         applicationContext.refresh();
         lookupObjectProvider(applicationContext);
+        lookupAvailable(applicationContext);
+
         applicationContext.close();
+
+    }
+
+    private static void lookupAvailable(AnnotationConfigApplicationContext applicationContext) {
+
+        ObjectProvider<User> userObjectProvider = applicationContext.getBeanProvider(User.class);
+        User user = userObjectProvider.getIfAvailable(() -> User.createUser());
+        System.out.println("当前 User 对象 : " + user);
 
     }
 
@@ -19,6 +30,7 @@ public class ObjectProviderDemo {
     public String helloWorld() {
         return "hello World";
     }
+
     private static void lookupObjectProvider(AnnotationConfigApplicationContext applicationContext) {
 
         ObjectProvider<String> provider = applicationContext.getBeanProvider(String.class);
